a27efbd98eac3aea036f3d2e7320370f31dc3278
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-08-28 17:41:34 +0900

    Create servlet_main.java

---
+++ src/servlet_main.java
@@ -0,0 +1,12 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class main extends HttpServlet {
+  
+  public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    
+    System.out.println("hi");
+  }
+}
+
