9df35078dd0754dd85bc093ce7d1c4d57942242a
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-09-04 02:26:41 +0900

    Update applicationObject.java

--- src/storeObject/applicationObject.java
+++ src/storeObject/applicationObject.java
@@ -11,12 +11,12 @@ public class main extends HttpServlet {
     response.setCharacterEncoding("UTF-8");
     response.setContentType("text/html; charset=UTF-8");
     
-    //하나의 요청 form에 여러번 입력이 발생
+    //하나의 요청 form에 여러번 입력이 발생 따라서 한페이지에 여러번 요청이 발생하는 경우 이전 값 임시 저장 방법
     String keyword = request.getParameter("keyword");
     int element = 0;
     if(!keyword.equals("")) element = Integer.parseInt(keyword);
     
-    ServletContext application request.getServletContext();
+    ServletContext application request.getServletContext(); //저장용 객체(어플리케이션)
     application.setAttribute("element", element); //key value 값으로 저장
     
     int tmp = (Integer)application.getAttribute("element"); //불러오기 {객체타입이라} Rapper 작업
