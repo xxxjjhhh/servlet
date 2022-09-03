f51961284298d5c44bec261c3331f707fc927a12
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-09-03 23:14:14 +0900

    Update filter.java

--- src/filter.java
+++ src/filter.java
@@ -10,6 +10,7 @@ public class servletFilter implements Filter {
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
     
+      chain.doFilter(request, response); // 다음 filter 또는 serlvet에 수행을 넘김 (함수 call) (미들웨어 개념)
   }
   
   @Override
@@ -17,3 +18,20 @@ public class servletFilter implements Filter {
     
   }
 }
+
+
+
+
+//Filter Setting
+
+-> xml : 
+<filter>
+  <filter-name>filtername1</filter-name>
+  <filter-class>package명.class명</filter-class>
+</filter>
+<filter-mapping>
+  <filter-name>filtername1</filter-name>
+  <url-pattern>/post</url-pattern>
+</filter-mapping>
+
+-> Annotation : 
