efd773fa3d3a41fb43665005f6e25e41805e7b27
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-09-07 00:40:40 +0900

    Update redirect.java

--- src/redirect.java
+++ src/redirect.java
@@ -1,1 +1,11 @@
-response.sendRedirect("/post");
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class main extends HttpServlet {
+  
+  public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    
+    response.sendRedirect("/post");
+  }
+}
