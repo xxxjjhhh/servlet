2bcea8f5cf0825642a93250ea3e2dd3705c7dd55
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-09-14 00:02:32 +0900

    Update connection.java

--- src/jdbc/connection.java
+++ src/jdbc/connection.java
@@ -6,12 +6,19 @@ import java.sql.DriverManager;
 public class Connect {
  
   public static void main(String[] args){
+   
+   String url = "jdbc:oracle:thin:@localhost:3306/servicename";
+   String sql = "SELECT * FROM topic";
     
     Class.forName("oracle.jdbc.driver.OracleDriver"); //jdbc 클래스 찾기
-    Connection con = DriverManager.getConnection();
+    Connection con = DriverManager.getConnection(주소, "id", "password");
     Statement st = con.createStatement(); //쿼리 입력
     ResultSet rs = st.executeQuery(sql);
     rs.next(); //다음 레코드
     String title = rs.getString("title");
+   
+   rs.close();
+   st.close();
+   con.close();
   }
 }
