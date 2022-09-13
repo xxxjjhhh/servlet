493db1e46da5dce5b6bb4817e2868dd53bb1565c
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-09-13 23:43:30 +0900

    Create connection.java

---
+++ src/jdbc/connection.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+
+public class Connect {
+ 
+  public static void main(String[] args){
+    
+    Class.forName("oracle.jdbc.driver.OracleDriver"); //jdbc 클래스 찾기
+    Connection con = DriverManager.getConnection();
+    Statement st = con.createStatement(); //쿼리 입력
+    ResultSet rs = st.executeQuery(sql);
+    rs.next(); //다음 레코드
+    String title = rs.getString("title");
+  }
+}
